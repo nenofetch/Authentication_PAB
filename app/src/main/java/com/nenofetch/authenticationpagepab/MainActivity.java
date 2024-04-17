70283fa60a098d1489a41b352b06e38a667c40bc
Neno Arisma <nenoarisma@gmail.com>
2024-04-17 20:20:27 +0700

    temp commit

--- app/src/main/java/com/nenofetch/authenticationpagepab/MainActivity.java
+++ app/src/main/java/com/nenofetch/authenticationpagepab/MainActivity.java
@@ -1,24 +1,55 @@
 package com.nenofetch.authenticationpagepab;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.nenofetch.authenticationpagepab.ui.login.LoginFragment;
+import com.nenofetch.authenticationpagepab.ui.login.RegisterFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginFragment, registerFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        loginFragment = findViewById(R.id.login_fragment_btn);
+        registerFragment = findViewById(R.id.register_fragment_btn);
+
+
+        loginFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new LoginFragment());
+            }
         });
+
+        registerFragment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                replaceFragment(new RegisterFragment());
+            }
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+
     }
 }
